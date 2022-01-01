2bb7e62346aa70736d71a0d16840f2b9fc08fc80
Scott Jones <scott2000jones@gmail.com>
2022-01-01 00:13:57 +0000

    Updated WezTerm config with VSCode Dark+ theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,18 @@
 local wezterm = require 'wezterm';
 return {
+  -- Colour theme based on Dark+ VSCode theme
+  -- See: https://github.com/dunstontc/atom-vscode-syntax/blob/master/styles/themes/dark-default.less
+  colors={
+    background="#1e1e1e",
+    foreground="#d4d4d4",
+    ansi = {
+        "#282c34", "#F44747", "#608B4E", "#D7BA7D", "#007ACC", "#646695", "#4EC9B0", "#D4D4D4"
+    },
+    brights = {
+        "#808080", "#D16969", "#B5CEA8", "#DCDCAA", "#569CD6", "#C586C0", "#9CDCFE", "white"
+    },
+  },
   font = wezterm.font("JetBrains Mono"),
-  color_scheme = "Guezwhoz",
   font_size = 18.0,
   enable_scroll_bar = true,
   keys = {
